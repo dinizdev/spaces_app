7266a7ec9ede556f23a78f1b9c779f7dd1b9cac8
Felipe Diniz <dinizdev@gmail.com>
2024-06-11 23:02:59 +0100

    add shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import SvgApp from '../assets/main_logo.svg'
+import Image from 'next/image'
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gradient-to-r from bg-primary to-bg-secondary ... flex h-screen justify-center">
       <h1>hello spaces</h1>
+      <Image src={SvgApp} alt="main logo"></Image>
     </div>
   )
 }
